1a9a2ca9876d4bc859b5f8e3c1a372b99532a0e9
Rohit <rohitpatidar0024@gmail.com>
2025-09-22 23:39:19 +0530

    Fix Vercel deployment configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,9 +12,8 @@ if (!PUBLISHABLE_KEY) {
 }
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl= '/'>
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ClerkProvider>
 )
